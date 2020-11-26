4c74f8e39973d91d0033bf3079775f28ebbeab07
karishma0806 <70712631+karishma0806@users.noreply.github.com>
2020-11-26 11:55:36 +0530

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,31 @@
+
+#1.
+
+l=[12,34,67,89]
+l.append(143)
+print(l)
+
+
+del l[3]
+print(l)
+
+
+m=[13,5,78,44,99,34,6,1]
+m.sort()
+print(m)
+print("largest number=",m[-1])#largest number in the list
+print("smallest number=",m[0])#smallest number in the list
+
+
+#2.
+t=(12,34,67,99,56)
+print(t[::-1])
+
+
+#3.
+
+t=(12,34,56,78,90)
+print(list(t))
+
+
+
