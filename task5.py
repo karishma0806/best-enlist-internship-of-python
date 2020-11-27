bb4434c603a9fd6434ad0c2597a5601ef170b180
karishma0806 <70712631+karishma0806@users.noreply.github.com>
2020-11-27 12:29:40 +0530

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,29 @@
+#1.
+
+def function(x,y):
+    sum=x+y
+    sub=x-y
+    div=x/y
+    mul=x*y
+    print("adding of two number",sum)
+    print("subtraction of two number",sub)
+    print("division  of two number",div)
+    print("multiplication  of two number",mul)
+function(3,2)
+
+#2.
+
+def covid():
+    pname=input("enter name")
+    default=98
+    temp=int(input("enter body temp"))
+    if temp<default:
+        temp=default
+    if temp>default:
+        temp=default
+    print(pname,temp)
+covid()
+        
+        
+
+
